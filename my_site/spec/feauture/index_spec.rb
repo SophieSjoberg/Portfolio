2edfe52b576faf021531f6a3c1f42dc210896381
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-13 15:44:47 +0100

    Add spec for partials

--- my_site/spec/feauture/index_spec.rb
+++ my_site/spec/feauture/index_spec.rb
@@ -1,7 +1,16 @@
 it 'displays project list' do
   expect(page).to have_css '.projects'
   within '.projects' do
-    expect(page).to have_content 'My First Website'
+    expect(page).to have_content 'ATM'
     expect(page).to have_content 'FizzBuzz'
+    expect(page).to have_content 'My First Website'
+  end
+end
+
+it 'renders footer partial' do
+  expect(page).to have_selector 'footer'
+  within 'footer' do
+    expect(page).to have_content 'Sophie\'s Portfolio'
+    expect(page).to have_content 'Saving lives one line of code at a time..'
   end
 end
