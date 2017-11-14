fe74233f9a9c1a30bba07c6eb5d4a38bbf7eb117
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-14 16:54:24 +0100

    Add ruby code that works with partials

--- my_site/spec/feauture/index_spec.rb
+++ my_site/spec/feauture/index_spec.rb
@@ -1,16 +1,25 @@
 it 'displays project list' do
   expect(page).to have_css '.projects'
   within '.projects' do
+    expect(page).to have_content 'Who am I?'
     expect(page).to have_content 'ATM'
     expect(page).to have_content 'FizzBuzz'
     expect(page).to have_content 'My First Website'
   end
 end
 
+it 'renders header partial' do
+  expect(page).to have_selector 'header'
+  within 'header' do
+    expect(page).to have_content
+    expect(page).to have_content 'SOPHIE\'S PORTFOLIO'
+  end
+end
+
 it 'renders footer partial' do
   expect(page).to have_selector 'footer'
   within 'footer' do
-    expect(page).to have_content 'Sophie\'s Portfolio'
+    expect(page).to have_content
     expect(page).to have_content 'Saving lives one line of code at a time..'
   end
 end
